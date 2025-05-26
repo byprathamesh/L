20ec49ff896d64d1f39bf918c79d82e7fcbd9ebb
byprathamesh <prathameshgavhane3@gmail.com>
2025-05-27 02:42:16 +0530

    Remove all chapter directories and practice sets

--- CHAPTER 2 PRACTICE SET/02_problem2.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main(){
-    float a = 3.0/8 -2;
-    printf("The calue of a is %f",a);
-    return 0;
-}
--- CHAPTER 2 PRACTICE SET/04_problem4.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main(){
-    int x=2 , y=3 , z=3 , k=1;
-    float e =3*x/y - z+k;
-    printf("The value of e is %f",e);
-    return 0;
-}
