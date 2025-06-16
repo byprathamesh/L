462f76ddeb512c82a5d6427bc9ad9aa947b1bb2f
byprathamesh <prathameshgavhane3@gmail.com>
2025-06-16 17:27:16 +0530

    hsvesvs

---
+++ C PROGRAMMING/PROJECT1/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    // Seed the random number generator with the current time
+    srand(time(0));
+
+    // Generate a random number between 1 and 100
+    int randomNumber = (rand() % 100) + 1;
+    int no_of_guesses = 0;
+    int guessed;
+
+    do {
+        printf("Guess the number: ");
+        scanf("%d", &guessed);
+        
+        if (guessed > randomNumber) {
+            printf("Lower number please!\n");
+        } else if (guessed < randomNumber) {
+            printf("Higher number please!\n");
+        }
+
+        no_of_guesses++;
+
+    } while (guessed != randomNumber);
+
+    printf("You guessed the number in %d guesses\n", no_of_guesses);
+
+    return 0;
+}
