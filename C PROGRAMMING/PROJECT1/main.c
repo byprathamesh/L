715dd6c691a95d653c1cff31e702e113fd051292
byprathamesh <prathameshgavhane3@gmail.com>
2025-06-16 17:43:48 +0530

    h

--- C PROGRAMMING/PROJECT1/main.c
+++ C PROGRAMMING/PROJECT1/main.c
@@ -9,23 +9,26 @@ int main() {
     // Generate a random number between 1 and 100
     int randomNumber = (rand() % 100) + 1;
     int no_of_guesses = 0;
-    int guessed;
+    int gussed_number;
 
     do {
         printf("Guess the number: ");
-        scanf("%d", &guessed);
+        scanf("%d", &gussed_number);
         
-        if (guessed > randomNumber) {
+        if (gussed_number > randomNumber) {
             printf("Lower number please!\n");
-        } else if (guessed < randomNumber) {
+        } else if (gussed_number < randomNumber) {
             printf("Higher number please!\n");
+        }
+        else{
+            printf("Congrats!!\n");
         }
 
         no_of_guesses++;
 
-    } while (guessed != randomNumber);
+    } while (gussed_number != randomNumber);
 
-    printf("You guessed the number in %d guesses\n", no_of_guesses);
+    printf("You gussed the number in %d guesses\n", no_of_guesses);
 
     return 0;
 }
